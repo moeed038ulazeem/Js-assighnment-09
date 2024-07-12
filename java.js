adead914de4a8852e2fc4542cf2bb3df44cc7129
moeed038ulazeem <moeedmoeed038@gamil.com>
2024-07-12 03:44:05 -0700

    assighnment-09

---
+++ java.js
@@ -0,0 +1,37 @@
+       //10th table
+//var number = prompt("please enter a number that you want to display it's table");
+//number = parseInt(number);
+//if (isNaN(number)) {
+  //  console.log("Invalid input ;;; please enter a valid number");
+//} else{
+  //  console.log(`10 time table for ${number}`);
+    //for (var i = 0; i <= 10; i++) {
+      //  console.log(`${number} * ${i} = ${number * i}`);
+    //}
+//}
+
+         //////SUM OF ARRAY ELEMENT
+
+//var number1 = prompt("enter the first number");
+//number1 = parseFloat(number1);
+//var number2 = prompt("please enter your second number");
+//number2 = parseFloat(number2);
+//var sum = number1 + number2;
+//alert(`The sum of ${number1} and ${number2} is: ${sum}`);
+//if (number1 > number2) {
+  //  alert("it is greater one")
+//} 
+
+
+// barha number
+
+
+//ULTA PRINT
+//var numbers = []
+//for (var i = 0; i < 5; i++){
+  //  numbers.push(+prompt(`enter your number`));
+//}
+
+//for (var j = numbers.length -1; j >= 0; j--){
+  //  console.log(numbers[j]);    
+//}
